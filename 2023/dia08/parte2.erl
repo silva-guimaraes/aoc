46dd1d1fb73cc530eda1fc739b4ca7ae0d1083fb
silva guimaraes <silva__guimaraes@hotmail.com>
2023-12-09 08:57:11 -0300

    doc

--- 2023/dia08/parte2.erl
+++ 2023/dia08/parte2.erl
@@ -1,6 +1,21 @@
 -module(parte2).
 -export([start/0]).
 
+% todas as sequencias de inicio e fim são desconectas.
+% convenientemente todas as sequencias entram e um loop depois de chegar no primero Z.
+% ainda mais conveniente é o fato de que o tamanho do loop e a distancia até o primeiro Z 
+% são iguais.
+% todas as sequencias possuem um tamanho de loop diferente.
+% ainda mutio mais conveniente é que todos os loops terminam em um numero multiplo do
+% tamanho das instruções, ou seja, o proximo loop fara o exato mesmo caminho e tera o
+% exato mesmo tamanho sempre.
+% basta usar minimo multiplo comum.
+% imagine 6 e 4, agora tente imaginar quais dois numeros podem multiplicar 6 e 4 para
+% que o produto dos dois seja o mesmo. 6x = 4y resumidamente.
+% esse era o problema em mãos
+% 
+% moral dessa história: verifiquem bem os seus inputs. sempre procure por padrões.
+
 
 map([], Map) -> Map;
 map([[Node | Edges]|T], Map) ->
@@ -29,6 +44,7 @@ find_z([H|T], LeftRight, Map, Current, Count) ->
 find_z(L, Start, Map) ->
     find_z(L, L, Map, Start, 0).
 
+% mesmo tendo dito aquilo essa não é a melhor imprementação de mmc...
 lcm(X, T, I) when (X*I) rem T == 0 -> X*I;
 lcm(X, T, I) -> lcm(X, T, I+1).
 lcm(X, T) -> lcm(X, T, 1).
