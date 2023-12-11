241307947d995168071f4cb06c940c071c9cc80a
silva guimaraes <silva__guimaraes@hotmail.com>
2023-12-11 01:59:48 -0300

    aaaaaaa

--- 2023/dia10/parte2.go
+++ 2023/dia10/parte2.go
@@ -1,12 +1,10 @@
 package main
 
-
 import (
 	"bufio"
 	"fmt"
 	"os"
-    "slices"
-    "os/exec"
+	"slices"
 )
 
 type pos struct {
@@ -17,7 +15,6 @@ type path struct {
     current pos
     number int
     before pos
-    // directions int
 }
 
 type connections []pos
@@ -26,8 +23,6 @@ type connections []pos
 
 func main() {
     file, err := os.Open("./input.txt")
-    // sumj := -1
-    sumj := 1
     if err != nil {
         panic(err)
     }
@@ -102,11 +97,8 @@ func main() {
         a := queue[0]
         queue = queue[1:]
 
-        for _, b := range visited {
-            if a.current == b {
-                goto end
-            }
-
+        if i := slices.Index(visited, a.current); i > -1 {
+            break
         }
 
         if a.current.i < 0 || a.current.i >= len(lines) ||
@@ -130,196 +122,29 @@ func main() {
 
         visited = append(visited, a.current)
     }
-    end:
-    var loop, temp []pos
-    for i := 0; i < len(visited); i += 2 {
-        loop = append(loop, visited[i])
-    }
-    for i := 1; i < len(visited); i += 2 {
-        temp = append(temp, visited[i])
-    }
 
-    slices.Reverse(temp)
-    loop = append(loop, temp...)
-    loop = append(loop, s)
-
-    directions := make([]pos, len(loop) - 1)
-
-    for i := 0; i < len(loop) -1 ; i++ {
-        directions[i] = pos{ loop[i+1].i - loop[i].i, loop[i+1].j - loop[i].j }
-    }
-
-    loop = loop[:len(loop)-1]
-
-    // fmt.Println()
-    // fmt.Println()
-
-    // return
-
-    // sumj == 1 esquerda -1 == direita
-    // sumj := loop[len(loop)-1].j - loop[0].j
-
-    var innerTiles []pos
-
-    for i := range loop {
-        // p1 := pos{67, 59}
-        var p pos
-
-        switch directions[i] {
-        case pos{ 1,  0}:
-            p = pos{loop[i].i, loop[i].j + sumj}
-        case pos{-1,  0}:
-            p = pos{loop[i].i, loop[i].j - sumj}
-        case pos{ 0, -1}:
-            p = pos{loop[i].i + sumj, loop[i].j}
-        case pos{ 0,  1}:
-            p = pos{loop[i].i - sumj, loop[i].j}
-        default:
-            panic(directions[i])
-        }
-
-        if i := slices.Index(loop, p); i > -1 {
-            continue
-        }
-        innerTiles = append(innerTiles, p)
-    }
-
-    // gambiarra adiante!
-    fuckYouRob := make(map[pos]byte)
-    for i := range innerTiles {
-        fuckYouRob[innerTiles[i]] = 0
-    } 
-    innerTiles = []pos{}
-    var bleed []pos
-    for k := range fuckYouRob {
-        bleed = append(bleed, k)
-    }
-    // innerTiles = bleed
-
-    for len(bleed) > 0 {
-        a := bleed[0]
-        bleed = bleed[1:]
-
-        if  a.i < 0 || a.i >= len(lines) || a.j < 0 || a.j >= len(lines[0]) {
-            continue
-        }
-
-        if i := slices.Index(loop, a); i > -1 {
-            continue
-        }
-
-        if i := slices.Index(innerTiles, a); i > -1 {
-            continue
-        }
-
-        bleed = append(bleed, pos{a.i+1, a.j})
-        bleed = append(bleed, pos{a.i, a.j+1})
-        bleed = append(bleed, pos{a.i-1, a.j})
-        bleed = append(bleed, pos{a.i, a.j-1})
-
-        innerTiles = append(innerTiles, a)
-    }
-    fmt.Println(len(innerTiles))
-
-    // return
-
-
-    cmd := exec.Command(
-        "ffmpeg", 
-        "-hide_banner",
-        "-y",
-        "-f", "rawvideo",
-        "-pix_fmt", "rgba",
-        "-s", fmt.Sprintf("%dx%d", len(lines[0]), len(lines)),
-        // "-r", fmt.Sprint(FPS),
-        // "-an",
-        "-i", "-", 
-        // "-vf", "scale=400:-1",
-        // "-c:v", "libx264",
-        "foobar.bmp",
-    )
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
-
-    stdin, err := cmd.StdinPipe()
-    if err != nil {
-        panic(err)
-    }
-
-    writer := bufio.NewWriter(stdin)
-
-    fmt.Println(cmd.String())
-    cmd.Start()
-
-    buf := make([]byte, len(lines) * len(lines[0]) * 4)
-
+    count := 0
     for i := range lines {
+        isInside := false
         for j := range lines[i] {
-            off := i*len(lines[0])*4 + j*4
-            // buf[off + 3] = 255
-            buf[off + 0] = 0
-            buf[off + 1] = 0
-            buf[off + 2] = 100
-            buf[off + 3] = 255
-        }
-    }
-    for _, p := range loop {
-        off := p.i*len(lines[0])*4 + p.j*4
-
-        // buf[off + 0] = 0
-        // buf[off + 1] = 255
-        // buf[off + 2] = 0
+            c := lines[i][j]
+            p := pos{i, j}
 
 
-        switch lines[p.i][p.j] {
-        case 'F':
-        buf[off + 0] = 255
-        buf[off + 1] = 100
-        buf[off + 2] = 0
-        case 'J':
-        buf[off + 0] = 100
-        buf[off + 1] = 255
-        buf[off + 2] = 100
-        case 'L':
-        buf[off + 0] = 100
-        buf[off + 1] = 100
-        buf[off + 2] = 255
-        case '7':
-        buf[off + 0] = 255
-        buf[off + 1] = 100
-        buf[off + 2] = 255
-        case '|':
-        buf[off + 0] = 255
-        buf[off + 1] = 255
-        buf[off + 2] = 100
-        case '-':
-        buf[off + 0] = 100
-        buf[off + 1] = 255
-        buf[off + 2] = 255
+            if idx := slices.Index(visited, p); idx > -1 {
+                if c == 'L' || c == '|' || c == 'J' || c == 'S' {
+                    isInside = !isInside
+                    continue
+                }
+                continue
+            }
+
+            if isInside {
+                count++
+            }
 
         }
-
-    }
-    for _, p := range innerTiles {
-        off := p.i*len(lines[0])*4 + p.j*4
-
-        buf[off + 0] = 255
-        buf[off + 1] = 0
-        buf[off + 2] = 0
-
     }
 
-    nn, err := writer.Write(buf); 
-    fmt.Println(nn)
-    if err != nil {
-        panic(err)
-    }
-
-
-    stdin.Close()
-
-    if cmd.Wait() != nil {
-        panic(err)
-    }
-
+    fmt.Println(count)
 }
