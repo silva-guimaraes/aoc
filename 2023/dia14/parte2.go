23c6d9fa286b2b14b8623f8fcb43670a210c18e4
silva guimaraes <silva__guimaraes@hotmail.com>
2023-12-14 13:47:23 -0300

    2023 14

--- 2023/dia14/parte2.go
+++ 2023/dia14/parte2.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
+    "bufio"
+    "fmt"
+    "os"
+    "strings"
+    "slices"
 )
 
 type pos struct {
@@ -12,7 +13,7 @@ type pos struct {
 }
 
 func main() {
-    file, err := os.Open("./teste.txt")
+    file, err := os.Open("./input.txt")
     if err != nil {
         panic(err)
     }
@@ -27,23 +28,33 @@ func main() {
     file.Close()
 
     cache := make(map[string][][]byte)
-    // cache["foo"] = [][]byte{}
 
-    // for times := 0; times < 1000000000; times++ {
+    var keys []string
 
-        // fmt.Print("\r", times)
+    for times := 0; times < 1000000000; times++ {
+
         var join []string
         for i := range lines {
             join = append(join, string(lines[i]))
         }
         key := strings.Join(join, "")
 
+        keys = append(keys, key)
+
         if ret, ok := cache[key]; ok {
             lines = ret
-            panic("repeat")
-            // continue
+            break
         }
 
+        copia := make([][]byte, len(lines))
+        for i := range copia {
+            copia[i] = make([]byte, len(lines[0]))
+            copy(copia[i], lines[i])
+        }
+
+        cache[key] = copia
+
+        // norte
         for ai := range lines {
             for aj, c := range lines[ai] {
                 if c == '.' || c == '#' { continue }
@@ -67,106 +78,97 @@ func main() {
             }
         }
 
-        // for aj := range lines[0] {
-        //     for ai := range lines {
-        //         c := lines[ai][aj]
-        //         if c == '.' || c == '#' { continue }
-        //         if aj == 0 { continue }
-        //         lines[ai][aj] = '.'
-        //         for j := aj-1; j >= 0; j-- {
-        //             if j == -1 {
-        //                 lines[ai][j+1] = 'O'
-        //                 break
-        //             }
-        //             ac := lines[ai][j]
-        //             if ac == '#' || ac == 'O' {
-        //                 lines[ai][j+1] = 'O'
-        //                 break
-        //             }
-        //             if j == 0 {
-        //                 lines[ai][j] = 'O'
-        //                 break
-        //             }
-        //         }
-        //     }
-        // }
-
-
-
-        // for aj := len(lines[0]) - 1; aj >= 0; aj-- {
-        //     for ai := range lines {
-        //         c := lines[ai][aj]
-        //         if c == '.' || c == '#' { continue }
-        //         if aj == len(lines[0]) - 1 { continue }
-        //         lines[ai][aj] = '.'
-        //         for j := aj+1; j < len(lines[0]); j++ {
-        //             if j == len(lines[0]) {
-        //                 lines[ai][j-1] = 'O'
-        //                 break
-        //             }
-        //             ac := lines[ai][j]
-        //             if ac == '#' || ac == 'O' {
-        //                 lines[ai][j-1] = 'O'
-        //                 break
-        //             }
-        //             if j == len(lines[0])-1 {
-        //                 lines[ai][j] = 'O'
-        //                 break
-        //             }
-        //         }
-        //     }
-        // }
-
-        // for ai := len(lines) - 1; ai >= 0; ai-- {
-        //     for aj, c := range lines[ai] {
-        //         if c == '.' || c == '#' { continue }
-        //         if ai == len(lines) - 1 { continue }
-        //         lines[ai][aj] = '.'
-        //         for i := ai+1; i >= 0; i++ {
-        //             if i == len(lines) {
-        //                 lines[i-1][aj] = 'O'
-        //                 break
-        //             }
-        //             ac := lines[i][aj]
-        //             if ac == '#' || ac == 'O' {
-        //                 lines[i-1][aj] = 'O'
-        //                 break
-        //             }
-        //             if i == len(lines)-1 {
-        //                 lines[i][aj] = 'O'
-        //                 break
-        //             }
-        //         }
-        //     }
-        // }
-
-        copia := make([][]byte, len(lines))
-        for i := range copia {
-            copia[i] = make([]byte, len(lines[0]))
-            copy(copia[i], lines[i])
+        // oeste
+        for aj := range lines[0] {
+            for ai := range lines {
+                c := lines[ai][aj]
+                if c == '.' || c == '#' { continue }
+                if aj == 0 { continue }
+                lines[ai][aj] = '.'
+                for j := aj-1; j >= 0; j-- {
+                    if j == -1 {
+                        lines[ai][j+1] = 'O'
+                        break
+                    }
+                    ac := lines[ai][j]
+                    if ac == '#' || ac == 'O' {
+                        lines[ai][j+1] = 'O'
+                        break
+                    }
+                    if j == 0 {
+                        lines[ai][j] = 'O'
+                        break
+                    }
+                }
+            }
         }
 
-        cache[key] = copia
-    // }
-    fmt.Println()
+        // sul
+        for ai := len(lines) - 1; ai >= 0; ai-- {
+            for aj, c := range lines[ai] {
+                if c == '.' || c == '#' { continue }
+                if ai == len(lines) - 1 { continue }
+                lines[ai][aj] = '.'
+                for i := ai+1; i >= 0; i++ {
+                    if i == len(lines) {
+                        lines[i-1][aj] = 'O'
+                        break
+                    }
+                    ac := lines[i][aj]
+                    if ac == '#' || ac == 'O' {
+                        lines[i-1][aj] = 'O'
+                        break
+                    }
+                    if i == len(lines)-1 {
+                        lines[i][aj] = 'O'
+                        break
+                    }
+                }
+            }
+        }
+
+        // leste
+        for aj := len(lines[0]) - 1; aj >= 0; aj-- {
+            for ai := range lines {
+                c := lines[ai][aj]
+                if c == '.' || c == '#' { continue }
+                if aj == len(lines[0]) - 1 { continue }
+                lines[ai][aj] = '.'
+                for j := aj+1; j < len(lines[0]); j++ {
+                    if j == len(lines[0]) {
+                        lines[ai][j-1] = 'O'
+                        break
+                    }
+                    ac := lines[ai][j]
+                    if ac == '#' || ac == 'O' {
+                        lines[ai][j-1] = 'O'
+                        break
+                    }
+                    if j == len(lines[0])-1 {
+                        lines[ai][j] = 'O'
+                        break
+                    }
+                }
+            }
+        }
+
+    }
+
+    a := keys[len(keys)-1]
+    b := keys[:len(keys)-1]
+    idx := slices.Index(b, a)
+
+    // ralei aqui mais do que na parte um inteira
+    lines = cache[keys[idx + ((1000000000 - idx) % (len(keys) - idx - 1))]]
 
     sum := 0
+
     for i := range lines {
         for _, c := range lines[i] {
-            fmt.Print(string(c)) 
             if c == 'O' {
                 sum += len(lines) - i
             }
         }
-        // fmt.Print("  ")
-        // for j, c := range lines[i] {
-        //     fmt.Print(string(copia[i][j])) 
-        //     if c == 'O' {
-        //         sum += len(lines) - i
-        //     }
-        // }
-        fmt.Println()
     }
-
     fmt.Println(sum)
 }
